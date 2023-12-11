9b078fc8f2fb44f1df76e564f3020b8b43e23cbd
max-wacquier <max.wacquier@gmail.com>
2023-12-11 09:59:19 +0100

    V1

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-print('Bonjour')
-print('Bonsoir')
+from datetime import datetime
 
 
-x = input('Enter text : ')
-print(x[::-1])
+def getCurrentTime():
+    now = datetime.now()
+    currentTime = now.strftime("%H")
+    return currentTime
+
+
+def sayHello():
+    currentTime = getCurrentTime()
+    if currentTime > '18':
+        print('Bonsoir')
+    else:
+        print('Bonjour')
+
+
+def isPalindrome(text):
+    if text == text[::-1]:
+        print('Bien dit !')
+
+
+def main():
+    sayHello()
+    x = input('Enter text : ')
+    isPalindrome(x)
+    print(x[::-1])
